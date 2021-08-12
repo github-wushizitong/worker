6f840ac70bb0b12e7ca13b6634501d1713fd77a9
github-wushizitong <ninganddell@outlook.com>
2021-08-12 12:47:23 +0800

    学习了自定义组件的生命周期函数和应用生命周期函数和页面生命周期函数

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,60 +2,27 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {
-    list: [
-      {
-        id: 0,
-        name: '首页',
-        isActive: true
-      },
-      {
-        id: 1,
-        name: '资讯',
-        isActive: false
-      },
-      {
-        id: 2,
-        name: '新闻',
-        isActive: false
-      },
-      {
-        id: 3,
-        name: '关于',
-        isActive: false
-      }
-    ]
-  },
-  handleItemClick(e) {
-    console.log(e);
-    // 获取事件源对象中的数据
-    let { index } = e.detail;
-    // 拷贝list 数据
-    let list = JSON.parse(JSON.stringify(this.data.list));
-    list.forEach((listItem, listIndex) => {
-      listIndex === index
-        ? (listItem.isActive = true)
-        : (listItem.isActive = false);
-    });
-    this.setData({
-      list: list
-    });
-  },
+  data: {},
 
   /**
    * 生命周期函数--监听页面加载
+   * 页面加载时触发。一个页面只会调用一次，可以在 onLoad 的参数中获取打开当前页面路径中的参数。
    */
-  onLoad: function (options) {},
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {},
+  onLoad: function (options) {
+    // 获取get请求的查询字符串参数
+    // console.log(options);
+    // onLoad 一般用于发送异步请求来初始化页面数据
+  },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {},
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面隐藏
@@ -64,21 +31,25 @@ Page({
 
   /**
    * 生命周期函数--监听页面卸载
+   * 关闭当前页面就属于卸载当前页面
    */
   onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
+   * 用于处理 页面的数据或者效果重新刷新
    */
   onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
+   * 用于加载下一页的数据
    */
   onReachBottom: function () {},
 
   /**
    * 用户点击右上角分享
+   * 当用户点击分享显示分享菜单的时候的回调
    */
   onShareAppMessage: function () {}
 });
